06ecf26ebbc1e2c0822d3a9f0b3b77405b19a8ca
PascalF44 <pascal.fievre@ssx-solutions.fr>
2018-07-29 11:20:37 +0200

    ajout du gravatar

--- react-hello/src/index.js
+++ react-hello/src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from "react-dom";
 import "./index.css";
 
 function Tweet() {
-  return <div className="tweet">Hello</div>;
+  return (
+    <div className="tweet">
+      <Avatar />
+      Hello
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img
+      src="https://gravatar.com/avatar/0321e6c633cd91e943cd0c1f07f4f5c7"
+      className="avatar"
+      alt="avatar"
+    />
+  );
 }
 
 ReactDOM.render(<Tweet />, document.querySelector("#root"));
